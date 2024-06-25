0d47b82d663fe40afdc3de0f7675fe111026e1a2
pauloFrancisconi <pauloffrancisconi@gmail.com>
2024-06-24 23:32:35 -0300

    [Feat] - Indices Criação dos índices para otimização das consultas SQL das perguntas de negócio

---
+++ scripts_sql/indices.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_movimentacoes_saida ON Movimentacoes(saida);
+
+CREATE INDEX idx_pagamentos_movimentacao_id ON Pagamentos(movimentacao_id);
+
+CREATE INDEX idx_veiculos_tipo_veiculo ON Veiculos(tipo_veiculo);
